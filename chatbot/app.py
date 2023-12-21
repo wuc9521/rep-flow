6f62b6491581f28cbf062499fd405cf7bd3fe755
wuc9521 <wuc9521@gmail.com>
2023-12-22 01:57:46 +0800

    finishing chatbot without image loading

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,5 +1,6 @@
 # Import necessary libraries
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask_cors import cross_origin
 import pandas as pd
 import spacy
 
@@ -18,23 +19,23 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
 @app.route('/ask', methods=['POST'])
+@cross_origin(supports_credentials=True)
 def ask():
-    user_input = request.form['user_input']
-    # Use spaCy to process user input
-    doc = nlp(user_input)
-
-    # Initialize response
-    response = "Sorry, I don't understand what you are asking."
-
-    # Look for a matching question in the QA pairs
-    for question, answer in qa.items():
-        if doc.similarity(nlp(question)) > 0.7:  # Set similarity threshold
-            response = answer
-            break
-
-    return render_template('index.html', user_input=user_input, response=response)
+    try:
+        data = request.get_json()
+        query_text = data['query']
+        
+        response = "Sorry, I don't understand what you are asking."
+        doc = nlp(query_text)
+        for question, answer in qa.items():
+            if doc.similarity(nlp(question)) > 0.7:  # Set similarity threshold
+                response = answer
+                break
+        return jsonify({"answer": response})
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({"error": "An error occurred"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
