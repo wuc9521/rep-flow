80a247c3388b184821c6772335442a1c179311ec
wuc9521 <wuc9521@gmail.com>
2023-12-21 23:10:11 +0800

    finishing frontend framework

--- chatbot/app.py
+++ chatbot/app.py
@@ -1,40 +1,35 @@
 # Import necessary libraries
 from flask import Flask, render_template, request
-
+import pandas as pd
 import spacy
 
 # Load spaCy English model
 nlp = spacy.load("en_core_web_sm")
 
-# Define a dictionary where keys are questions and values are answers
-qa_pairs = {
-    "你叫什么名字？": "我是ChatGPT，您的聊天机器人。",
-    "你会做什么？": "我可以回答一些基本问题。",
-    "如何学习编程？": "学习编程需要不断练习，阅读文档，参加课程，解决问题，不断学习。"
-}
+df = pd.read_csv('./data/qa.csv')
+print(df.columns)
+qa = dict(zip(df['Q'], df['A']))
 
 # Initialize Flask application
 app = Flask(__name__)
 
-# Define a route for the home page
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Define a route for handling user queries
+
 @app.route('/ask', methods=['POST'])
 def ask():
-    # Get user input from the form
     user_input = request.form['user_input']
-
     # Use spaCy to process user input
     doc = nlp(user_input)
 
     # Initialize response
-    response = "我不明白您的问题。"
+    response = "Sorry, I don't understand what you are asking."
 
     # Look for a matching question in the QA pairs
-    for question, answer in qa_pairs.items():
+    for question, answer in qa.items():
         if doc.similarity(nlp(question)) > 0.7:  # Set similarity threshold
             response = answer
             break
@@ -43,4 +38,3 @@ def ask():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
