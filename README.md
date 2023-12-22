d2153d44083d162ee07e3f40411ccb569b2acbfe
wuc9521 <wuc9521@gmail.com>
2023-12-23 01:06:11 +0800

    successfully fetching page xml data

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
 # GUI-bug-reproduction
 A possible implementation of paper "[Towards Effective Bug Reproduction for Mobile Applications](https://ieeexplore.ieee.org/document/10314157)"
 
-# Run
+
+
+# Prerequisites
 
 ```shell
 pip3 install -r requirements.txt
+npm install
+```
+
+# Quick Start
+
+```shell
+appium
 ```
 
 ```shell
 cd chatbot
 python3 app.py
 ```
+
+```shell
+cd script
+python3 main.py
+```
+
--- script/main.py
+++ script/main.py
@@ -1,54 +1,57 @@
 from appium import webdriver
-from appium.options.android import UiAutomator2Options
+import subprocess as sp
 import time
+from datetime import datetime
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.appium_service import AppiumService
+
+ip_address = '127.0.0.1'
+port = '4725'
+date = datetime.now().strftime("%Y%m%d%H%M")
+
+# Run Appium server, store logfile
+appium_service = AppiumService()
+appium_service.start(args=[
+    '--address', ip_address,
+    '-p', port,
+    '--log-timestamp',
+    '--log', './logs/{}.appium.log'.format(date),
+], stdout=sp.DEVNULL)
+
+print("appium is running : ", appium_service.is_running)  # return True, ok
+print("appium is listening : ", appium_service.is_listening)  # return True, ok
 
 options = UiAutomator2Options()
+desired_caps = {
+    "platformName": "Android",
+    "platformVersion": "13",
+    "deviceName": "Android Emulator",
+    "appPackage": "com.google.android.calculator",
+    "appActivity": "com.android.calculator2.Calculator"
+}
+options.set_capability('appium:chromeOptions', desired_caps)
 
-_options = {}
-_options['platformName'] = 'Android'
-_options['platformVersion'] = '13'
-_options['deviceName'] = 'Android Emulator'
-_options['appPackage'] = 'tv.danmaku.bili'
-_options['appActivity'] = '.MainActivityV2'
-_options['automationName'] = 'UiAutomator2'
-_options['language'] = 'en'
-_options['locale'] = 'US'
-_options['noReset'] = True
-options.set_capability('appium:chromeOptions', {'w3c': False})
 
-# capabilities = dict( 
-#     platformName='Android',
-#     automationName='UiAutomator2',
-#     deviceName='emulator-5554',
-#     appPackage='tv.danmaku.bili',
-#     appActivity='.MainActivityV2',
-#     language='en',
-#     locale='US',
-#     noReset=True
-# )
-
-# Appium 服务器地址
-server = 'http://127.0.0.1:4723/wd/hub'
+# appium 1-->2: no need to add /wd/hub to the end of the url
+# appium 2: no slash at the end of the url
 driver = webdriver.Remote(
-    command_executor=server,
-    options=options,
+    command_executor='http://{}:{}'.format(ip_address, port), 
+    options=options
 )
 
 
-# 获取当前页面的 XML 结构
+print("appium is connected to the device")  # Nothing is printed, the script
+
 def get_current_page_source():
     xml_string = driver.page_source
-    xml_path = f'./fetch/screenshot_{time.time()}.xml'
+    xml_path = f'./xml/{time.time()}.appium.xml'
     with open(xml_path, 'w', encoding='utf-8') as xml_file:
         xml_file.write(xml_string)
     print(f'Page source saved to: {xml_path}')
 
-# 在这里监听用户点击事件
-# 这只是一个示例，具体实现会依赖于你的设备和操作系统
-# 你可能需要使用第三方库或工具来监听设备的输入事件
 
 # 等待一段时间，保持脚本运行
-time.sleep(30)
+time.sleep(10)
 
 # 获取当前页面的 XML 结构
 get_current_page_source()
