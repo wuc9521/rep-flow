00880781262b5987d0c1d97c1481528938722166
wuc9521 <wuc9521@gmail.com>
2023-12-23 17:40:34 +0800

    adding event driven functions

--- script/main.py
+++ script/main.py
@@ -1,22 +1,10 @@
 import time
 import subprocess as sp
 from appium import webdriver
-from appium.webdriver.extensions.session import Session
 from datetime import datetime
+from appium.common.logger import logger
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.appium_service import AppiumService
-from appium.webdriver.webdriver import ExtensionBase
-
-class EventListener(ExtensionBase):
-    def method_name(self):
-        return 'custom_method_name'
-
-    def test_command(self, argument):
-        return self.execute(argument)['value']
-
-    def add_command(self):
-        return ('post', 'session/$sessionId/path/to/your/custom/url')
-
 
 def start_appium_server():
     ip_address = '127.0.0.1'
@@ -32,10 +20,7 @@ def start_appium_server():
         '--log', './log/{}.appium.log'.format(date),
         '--use-plugins='.format("images"),
     ], stdout=sp.DEVNULL)
-
     print("appium is running : ", appium_service.is_running)
-    print("appium is listening : ", appium_service.is_listening)
-
     return ip_address, port
 
 
@@ -45,8 +30,6 @@ def initialize_driver(ip_address, port):
         "platformName": "Android",
         "platformVersion": "13",
         "deviceName": "Android Emulator",
-        "appPackage": "com.google.android.calculator",
-        "appActivity": "com.android.calculator2.Calculator"
     }
     options.set_capability('appium:chromeOptions', desired_caps)
 
@@ -55,34 +38,46 @@ def initialize_driver(ip_address, port):
     driver = webdriver.Remote(
         command_executor='http://{}:{}'.format(ip_address, port),
         options=options,
-        extensions=[EventListener]
     ),
-    
-    print("appium is connected to the device")
+
+    print("appium connected to the device")
     return driver
 
 
 def get_current_page_source(driver):
     xml_string = driver.page_source
-    xml_path = f'./xml/{time.time()}.appium.xml'
-    with open(xml_path, 'w', encoding='utf-8') as xml_file:
-        xml_file.write(xml_string)
-    print(f'Page source saved to: {xml_path}')
+    return xml_string
 
+def capture_screenshot(driver, screenshot_path):
+    driver.save_screenshot(screenshot_path)
+    print(f'Screenshot saved to: {screenshot_path}')
 
+def is_button_pressed(driver):
+    try:
+        buttons = driver.find_elements_by_xpath("//Button")
+        for button in buttons:
+            if button.is_enabled() and button.is_displayed():
+                return True
+        return False
+    except:
+        return False
+    
 def main():
     ip_address, port = start_appium_server()
     driver = initialize_driver(ip_address, port)
+    driver = driver[0]
+    previous_page_source = get_current_page_source(driver)
     try:
         while True:
-            # 获取当前的session
-
-            time.sleep(1)
+            current_page_source = get_current_page_source(driver)
+            if current_page_source != previous_page_source:
+                screenshot_path = f'../data/img/{time.time()}.png'
+                capture_screenshot(driver, screenshot_path)
+                previous_page_source = current_page_source
+            time.sleep(0.1)
     except KeyboardInterrupt:
-        # 捕获 Ctrl+C 信号，退出循环
         print("Exiting the loop.")
     finally:
-        # 关闭 Appium 会话
         driver.quit()
 
 
