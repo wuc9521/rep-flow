19367795933c1576785586e177847ac27afcfcd5
wuc9521 <wuc9521@gmail.com>
2023-12-31 22:22:58 +0800

    finishing hint support

--- utils/hints.py
+++ utils/hints.py
@@ -1,7 +1,6 @@
-HELP = "\
-    <ul class='hint-font' onclick='handleHintClick(event)'> \
-        <li><span>show help:</span><u>/help</u> </li> \
-        <li><span>test bug:</span> <u>/test $BUG</u></li> \
-        <li><span>show current state:</span> <u>/state</u></li> \
-    </ul> \
+HELP = "<ul class='hint-font' onclick='handleHintClick(event)'> \
+                            <li><span>show help:</span><u>/help</u> </li> \
+                            <li><span>test bug:</span> <u>/test $BUG</u></li> \
+                            <li><span>show current state:</span> <u>/state</u></li> \
+                        </ul> \
 "
